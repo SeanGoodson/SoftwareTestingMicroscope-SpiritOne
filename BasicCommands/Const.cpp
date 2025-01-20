d5b3ef9cde04d3942d6d2156f7b9e1f97ce16ca1
Seaniepants <seaniepants77@gmail.com>
2025-01-19 23:49:51 -0500

    updated UI

---
+++ BasicCommands/Const.cpp
@@ -0,0 +1,139 @@
+#include "Const.h"
+
+namespace WindowConstants {
+	int	WindowConstants::windowWidth()
+	{
+		return 800;
+	}
+	int	WindowConstants::windowHeight()
+	{
+		return 700;
+	}
+}
+
+namespace MultimeterIndicatorConstants {
+	int MultimeterIndicatorConstants::xIndicator()
+	{
+		return 20;
+	}
+
+	int MultimeterIndicatorConstants::yIndicator()
+	{
+		return 50;
+	}
+
+	int MultimeterIndicatorConstants::WidthIndicator()
+	{
+		return 300;
+	}
+
+	int MultimeterIndicatorConstants::HeightIndicator()
+	{
+		return 25;
+	}
+}
+namespace PowermeterIndicatorConstants {
+	int PowermeterIndicatorConstants::xIndicator()
+	{
+		return 20;
+	}
+
+	int PowermeterIndicatorConstants::yIndicator()
+	{
+		return 100;
+	}
+
+	int PowermeterIndicatorConstants::WidthIndicator()
+	{
+		return 300;
+	}
+
+	int PowermeterIndicatorConstants::HeightIndicator()
+	{
+		return 25;
+	}
+}
+namespace LabelMultimeterConstants {
+	int LabelMultimeterConstants::xLabel()
+	{
+		return 20;
+	}
+
+	int LabelMultimeterConstants::yLabel()
+	{
+		return 25;
+	}
+
+	int LabelMultimeterConstants::WidthLabel()
+	{
+		return 300;
+	}
+
+	int LabelMultimeterConstants::HeightLabel()
+	{
+		return 25;
+	}
+}
+namespace LabelPowermeterConstants {
+	int LabelPowermeterConstants::xLabel()
+	{
+		return 20;
+	}
+
+	int LabelPowermeterConstants::yLabel()
+	{
+		return 75;
+	}
+
+	int LabelPowermeterConstants::WidthLabel()
+	{
+		return 300;
+	}
+
+	int LabelPowermeterConstants::HeightLabel()
+	{
+		return 25;
+	}
+}
+namespace NeutralDensityStageIndicatorConstants {
+	int NeutralDensityStageIndicatorConstants::xIndicator()
+	{
+		return 20;
+	}
+
+	int NeutralDensityStageIndicatorConstants::yIndicator()
+	{
+		return 150;
+	}
+
+	int NeutralDensityStageIndicatorConstants::WidthIndicator()
+	{
+		return 300;
+	}
+
+	int NeutralDensityStageIndicatorConstants::HeightIndicator()
+	{
+		return 25;
+	}
+}
+namespace LabelNeutralDensityStageConstants {
+	int LabelNeutralDensityStageConstants::xLabel()
+	{
+		return 20;
+	}
+
+	int LabelNeutralDensityStageConstants::yLabel()
+	{
+		return 125;
+	}
+
+	int LabelNeutralDensityStageConstants::WidthLabel()
+	{
+		return 300;
+	}
+
+	int LabelNeutralDensityStageConstants::HeightLabel()
+	{
+		return 25;
+	}
+}
---
+++ BasicCommands/Const.h
@@ -0,0 +1,65 @@
+#pragma once
+namespace WindowConstants {
+	class WindowConstants {
+	public:
+		int windowWidth();
+		int windowHeight();
+
+	};
+}
+namespace MultimeterIndicatorConstants {
+	class MultimeterIndicatorConstants {
+	public:
+		int xIndicator();
+		int yIndicator();
+		int WidthIndicator();
+		int HeightIndicator();
+
+	};
+}
+namespace PowermeterIndicatorConstants {
+	class PowermeterIndicatorConstants {
+	public:
+		int xIndicator();
+		int yIndicator();
+		int WidthIndicator();
+		int HeightIndicator();
+
+	};
+}
+namespace LabelMultimeterConstants {
+	class LabelMultimeterConstants {
+	public:
+		int xLabel();
+		int yLabel();
+		int WidthLabel();
+		int HeightLabel();
+	};
+}
+namespace LabelPowermeterConstants {
+	class LabelPowermeterConstants {
+	public:
+		int xLabel();
+		int yLabel();
+		int WidthLabel();
+		int HeightLabel();
+	};
+}
+namespace NeutralDensityStageIndicatorConstants {
+	class NeutralDensityStageIndicatorConstants {
+	public:
+		int xIndicator();
+		int yIndicator();
+		int WidthIndicator();
+		int HeightIndicator();
+	};
+}
+namespace LabelNeutralDensityStageConstants {
+	class LabelNeutralDensityStageConstants {
+	public:
+		int xLabel();
+		int yLabel();
+		int WidthLabel();
+		int HeightLabel();
+	};
+}
